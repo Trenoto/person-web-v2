b36c9169444e83a04f154ac171b56cfdeae2e949
Hongtao Liu <faberaldliu@gmail.com>
2019-03-02 19:22:03 +0800

    Add carousel to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
+import React, { Component } from 'react';
 import styles from './index.less';
 import { Carousel } from 'antd';
-import { formatMessage } from 'umi-plugin-locale';
 
-export default function() {
-  return (
-<Carousel autoplay autoplaySpeed="62" effect="fade" className={styles.carousel}>
-    <div className={styles.carouselImage1}></div>
-    <div className={styles.carouselImage2}></div>
-    <div className={styles.carouselImage3}></div>
-  </Carousel>
-  );
+export default class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sloganBlack: false,
+    }
+  }
+
+   setSloganColor = (index) => {
+     if (index === 1) {
+      this.setState({
+        sloganBlack: true,
+      });
+     } else {
+      this.setState({
+        sloganBlack: false,
+      });
+     }
+  };
+
+  render() {
+    return (
+      <div>
+        <div className={styles.wrap}>
+          <div className={this.state.sloganBlack ? styles.sloganBlack : styles.slogan}></div>
+        </div>
+        <Carousel autoplay autoplaySpeed="52" effect="fade" className={styles.carousel} beforeChange={ (current, next) => this.setSloganColor(next)}>
+          <div className={styles.carouselImage1}></div>
+          <div className={styles.carouselImage2}></div>
+          <div className={styles.carouselImage3}></div>
+        </Carousel>
+      </div>
+    );
+  }
 }
