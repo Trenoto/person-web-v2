b18c55c6c76b2948eed762a0d68cdc19a0fe0150
Hongtao Liu <faberaldliu@gmail.com>
2019-03-04 20:47:10 +0800

    Update header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import { Layout, Menu } from 'antd';
 import Link from 'umi/link';
 import styles from './Header.less';
+import logo from '../assets/logo.svg';
 
 const AntHeader = Layout.Header;
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navBarTextIsTransparent: false,
+        }
+        this.setNarBarTextStyle.bind(this);
+    }
+
+    setNarBarTextStyle() {
+        let backGroundColor = document.body.style.backGroundColor;
+        if (backGroundColor === "rgb(255, 255, 255)") {
+            this.setState({
+                navBarTextIsTransparent: false
+            });
+        } else {
+            this.setState({
+                navBarTextIsTransparent: true
+            });
+        }
+    }
+
+    componentDidMount() {
+        this.setNarBarTextStyle();
+    }
     render() {
         return (
             <AntHeader className={styles.header} >
@@ -15,9 +40,10 @@ class Header extends Component {
                     defaultSelectedKeys={['2']}
                     className={styles.menu}
                 >
-                    <Menu.Item className={styles.navbarItem} key="about"><Link to="/about">about</Link></Menu.Item>
-                    <Menu.Item className={styles.navbarItem} key="contact"><Link to="/contact">contact</Link></Menu.Item>
-                    <Menu.Item className={styles.navbarItem} key="blog"><Link to="/blog">blog</Link></Menu.Item>
+                    <Menu.Item className={styles.navbarItemLeft} key="logo"><Link to="/"><img alt="logo" className={styles.logo} src={logo} /></Link></Menu.Item>
+                    <Menu.Item className={styles.navbarItemRight} key="about"><Link to="/about">about</Link></Menu.Item>
+                    <Menu.Item className={styles.navbarItemRight} key="contact"><Link to="/contact">contact</Link></Menu.Item>
+                    <Menu.Item className={styles.navbarItemRight} key="blog"><Link to="/blog">blog</Link></Menu.Item>
                 </Menu>
             </AntHeader>
         );
