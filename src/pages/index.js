bec49e812b3610570554049fc2da7f593c59c80e
Hongtao Liu <faberaldliu@gmail.com>
2019-03-04 20:47:25 +0800

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default class IndexPage extends Component {
         <div className={styles.wrap}>
           <div className={this.state.sloganBlack ? styles.sloganBlack : styles.slogan}></div>
         </div>
-        <Carousel autoplay autoplaySpeed="52" effect="fade" className={styles.carousel} beforeChange={ (current, next) => this.setSloganColor(next)}>
+        <Carousel autoplay autoplaySpeed="62" effect="fade" className={styles.carousel} beforeChange={ (current, next) => this.setSloganColor(next)}>
           <div className={styles.carouselImage1}></div>
           <div className={styles.carouselImage2}></div>
           <div className={styles.carouselImage3}></div>
