c4a409b84eaa1c3bfe727aa66650d37b77ba5323
Hongtao Liu <faberaldliu@gmail.com>
2019-03-07 13:33:25 +0800

    Change header text color when path changes

---
+++ src/pages/about/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styles from './index.less';
+
+export default class AboutPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: "rgb(255, 255 ,255)",
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        safs
+      </div>
+    );
+  }
+}
